05496c3b0d91f43a40f834ec0a7a0d72e7bff03a
lilbovio <lilbovio30@gmail.com>
2026-06-06 20:32:37 -0600

    feat: Adding speed insights into my portfolio

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
